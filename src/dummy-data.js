7211eb3e48a8f9ca9c4ec9e62387a9f152f25740
samantha <samanthapelkova1@gmail.com>
2023-09-26 16:12:58 +0200

    adding two more users

--- src/dummy-data.js
+++ src/dummy-data.js
@@ -29,5 +29,21 @@ export default [
     id: 'michael',
     fullName: 'Michael Miller',
     projects: []
-  }
+  },
+  {
+    id: 'josef',
+    fullName: 'Josef Jones',
+    projects: [
+      { id: 'josef_p1', title: 'Create more courses' },
+      { id: 'josef_p2', title: 'Explore new topics' }
+    ]
+  },
+  {
+    id: 'marta',
+    fullName: 'Marta Jones',
+    projects: [
+      { id: 'marta_p1', title: 'Create agenda for next months' },
+      { id: 'marta_p2', title: 'Explore new topics' }
+    ]
+  },
 ];
